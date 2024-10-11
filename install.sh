e9c8bf748c4998fd16f0c00d983874307c0ddc3d
Alexander Sidorenko <alexander@sidorenko.xyz>
2024-10-11 11:54:41 -0700

    [shrc] Bootstrapping bashrc and zshrc infrastructure.
    Adding `shrc` that holds Run Commands (RC) that are common to both Bash and Zsh, as well as separate `bashrc`/`zshrc` files that can hold commands specific to these shells.
    
    `install.sh` script added that makes `~/.bashrc` and `~/.zshrc` source my RC files.

---
+++ install.sh
@@ -0,0 +1,5 @@
+# Install my bashrc
+echo "source ~/.sidorenko_dotfiles/bashrc" >> ~/.bashrc
+
+# Install my zshrc
+echo "source ~/.sidorenko_dotfiles/zshrc" >> ~/.zshrc
---
+++ zshrc
@@ -0,0 +1,4 @@
+# Run commands that are common to zshrc and bashrc
+source ~/.sidorenko_dotfiles/shrc
+
+# Run zsh-specific commands
