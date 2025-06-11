28f912886cefcc7e5d819373f437855770abc184
Alexander Sidorenko <alexander@sidorenko.xyz>
2025-06-11 14:29:14 +0300

    [gitconfig] Adding personal gitconfig and harnessing to install it.

--- install.sh
+++ install.sh
@@ -3,3 +3,10 @@ echo "source ~/.sidorenko_dotfiles/bashrc" >> ~/.bashrc
 
 # Install my zshrc
 echo "source ~/.sidorenko_dotfiles/zshrc" >> ~/.zshrc
+
+# Install .gitconfig
+{
+    echo ""
+    echo "[include]"
+    echo "    path = ~/.sidorenko_dotfiles/gitconfig.personal"
+} >> "$HOME/.gitconfig"
