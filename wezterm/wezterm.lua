cad847543087f3a94a47de9c1a9075a85961208d
Alexander Sidorenko <alexander@sidorenko.xyz>
2026-02-06 13:32:44 -0800

    [wezterm] Bumping DPI to 160.
    It is too small on macOS. Might need to tweak it futher/per setup.

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -4,7 +4,7 @@ local config = wezterm.config_builder and wezterm.config_builder() or {}
 
 local font_name = "DroidSansM Nerd Font"
 local font_size = 14.0
-local font_dpi = 96.0
+local font_dpi = 160
 
 config.font = wezterm.font(font_name, { weight = "Regular" })
 config.font_size = font_size
